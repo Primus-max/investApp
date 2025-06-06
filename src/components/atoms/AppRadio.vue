0054f35cb18cd8200d883c35f6ad2345c643400b
Max Pares <urkytsk3@gmail.com>
2025-06-06 16:16:09 +0300

    Обновлен компонент AppRadio: заменен визуальный элемент для состояния выбора с точки на галочку, добавлен SVG для отображения галочки. Изменены стили для нового элемента и улучшена позиция отображения.

--- src/components/atoms/AppRadio.vue
+++ src/components/atoms/AppRadio.vue
@@ -12,7 +12,12 @@
       @change="$emit('update:modelValue', value)"
     />
     <span class="app-radio__circle">
-      <span v-if="modelValue === value" class="app-radio__dot"></span>
+      <span v-if="modelValue === value" class="app-radio__check">
+        <svg width="24" height="24" viewBox="0 0 24 24">
+          <circle cx="12" cy="12" r="12" fill="#1B43E5" />
+          <polyline points="7,13 11,17 17,9" fill="none" stroke="#fff" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+      </span>
     </span>
     <span class="app-radio__label">
       <slot />
