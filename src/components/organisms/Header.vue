9f802ca9ba5fcd46a078a51dab77e43155dc2d37
Max Pares <urkytsk3@gmail.com>
2025-06-06 12:13:41 +0300

    Добавлены новые SVG-иконки: heart, star, bell, и обновлен файл icons-sprite.svg для их поддержки. Исправлены ошибки в определениях иконок и улучшено форматирование кода.

---
+++ src/components/organisms/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="header">
+    <div class="header__icon">
+      <slot name="left" />
+    </div>
+    <span class="header__title">
+      <slot />
+    </span>
+    <div class="header__action">
+      <slot name="right" />
+    </div>
+  </header>
+</template>
+
+<script setup>
+// No props, only slots
+</script>
+
+<style scoped lang="scss">
+@import '@/styles/organisms/header.scss';
+</style> 
