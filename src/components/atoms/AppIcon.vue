07b52519d3e204121d04301586bd828d5e8aa6e6
Max Pares <urkytsk3@gmail.com>
2025-06-06 09:53:28 +0300

    Добавлено подключение шрифта Inter в index.html и обновлены стили в main.scss с импортом переменных и миксинов, а также изменены базовые стили для html и body.

---
+++ src/components/atoms/AppIcon.vue
@@ -0,0 +1,33 @@
+<template>
+  <component :is="iconComponent" class="app-icon" v-if="iconComponent" />
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+// Импортируем SVG-иконки (добавим реальные файлы после экспорта из Figma)
+import BtcIcon from '@/assets/icons/btc.svg';
+import EthIcon from '@/assets/icons/eth.svg';
+
+// ...другие импорты по мере добавления
+
+const props = defineProps({
+  name: { type: String, required: true }
+})
+
+const icons = {
+  btc: BtcIcon,
+  eth: EthIcon,
+  // ...другие иконки
+}
+
+const iconComponent = computed(() => icons[props.name] || null)
+</script>
+
+<style scoped lang="scss">
+.app-icon {
+  width: 32px;
+  height: 32px;
+  display: inline-block;
+}
+</style> 
