7978db57aa32da27e2ef870f7653ab7bfd0d299d
Max Pares <urkytsk3@gmail.com>
2025-06-10 18:15:41 +0300

    Обновлены параметры графика в компоненте LineChartAtom.vue: добавлено свойство width для линии, упрощены настройки оси X, удалены неиспользуемые категории. Улучшена визуализация графика.

--- src/components/atoms/stats/LineChartAtom.vue
+++ src/components/atoms/stats/LineChartAtom.vue
@@ -23,25 +23,16 @@ const chartOptions = {
     type: 'area',
     height: 120,
     toolbar: { show: false },
-    background: 'transparent'
+    background: 'transparent',   
   },
   dataLabels: {
     enabled: false
   },
   stroke: {
-    curve: 'smooth'
+    curve: 'smooth',
+    width: 2
   },
   xaxis: {
-    type: 'datetime',
-    categories: [
-      "2018-09-19T00:00:00.000Z",
-      "2018-09-19T01:30:00.000Z",
-      "2018-09-19T02:30:00.000Z",
-      "2018-09-19T03:30:00.000Z",
-      "2018-09-19T04:30:00.000Z",
-      "2018-09-19T05:30:00.000Z",
-      "2018-09-19T06:30:00.000Z"
-    ],
     labels: { show: false },
     axisBorder: { show: false },
     axisTicks: { show: false }
