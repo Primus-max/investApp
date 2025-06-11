8f9656be82124ddc5bbaff67f264050ba0a61454
Max-Pares <79203161+Primus-max@users.noreply.github.com>
2025-06-11 10:11:25 +0300

    Updated StatWidgetCard.vue to handle empty data state: added logic to display a "No Data" badge and adjusted styles for better visualization. Enhanced computed properties for data checks and modified layout for improved user experience.

--- src/components/molecules/stat-widgets/StatWidgetCard.vue
+++ src/components/molecules/stat-widgets/StatWidgetCard.vue
@@ -5,32 +5,37 @@
     </div>
     <div class="stat-widget-card__title">{{ title }}</div>
     <div
-      :class="['stat-widget-card__content', positive ? 'stat-widget-card__content--positive' : 'stat-widget-card__content--negative']">
+      :class="['stat-widget-card__content', positive && !isNotData ? 'stat-widget-card__content--positive' : 'stat-widget-card__content--negative', isNotData ? 'stat-widget-card__content--empty' : '']">
       <div
-        :class="['stat-widget-card__chart-bg', positive ? 'stat-widget-card__chart-bg--positive' : 'stat-widget-card__chart-bg--negative']">
-        <LineChartAtom :data="chartData" :positive="positive" :color="positive ? '#3CB46E' : '#F04438'" :width="167"
-          :height="120" />
+        :class="['stat-widget-card__chart-bg', positive && !isNotData ? 'stat-widget-card__chart-bg--positive' : 'stat-widget-card__chart-bg--negative', isNotData ? 'stat-widget-card__chart-bg--empty' : '']">
+        <IconEmptyStat v-if="isNotData" />
+        <LineChartAtom v-else :data="chartData" :positive="positive" :color="positive ? '#3CB46E' : '#F04438'"
+          :width="167" :height="120" />
       </div>
       <div class="stat-widget-card__info">
         <div class="stat-widget-card__value-row">
-          <span
+          <span v-if="!isNotData"
             :class="['stat-widget-card__sign', positive ? 'stat-widget-card__sign--positive' : 'stat-widget-card__sign--negative']">
             {{ positive ? '+' : '-' }}
           </span>
-          <span class="stat-widget-card__value">{{ value }}</span>
+          <span class="stat-widget-card__value">{{ isNotData ? 0 : value }}</span>
           <span class="stat-widget-card__currency">₽</span>
         </div>
         <div class="stat-widget-card__percent-row">
-          <span
-            :class="['stat-widget-card__percent', positive ? 'stat-widget-card__percent--positive' : 'stat-widget-card__percent--negative']">
+          <span v-if="!isNotData"
+            :class="['stat-widget-card__percent', positive && !isNotData ? 'stat-widget-card__percent--positive' : 'stat-widget-card__percent--negative']">
             {{ percent }}%
           </span>
-          <span
+          <span v-if="!isNotData"
             :class="['stat-widget-card__arrow', positive ? 'stat-widget-card__arrow--positive' : 'stat-widget-card__arrow--negative']">
-            <template v-if="positive">↑</template>
+            <template v-if="positive && !isNotData">↑</template>
             <template v-else>↓</template>
           </span>
         </div>
+      </div>
+      <!-- <div v-else class="stat-widget-card__info">0 <span class="stat-widget-card__currency">₽</span></div> -->
+      <div v-if="isNotData" class="stat-widget-card__empty-center">
+        <div class="stat-widget-card__empty-badge">Нет данных</div>
       </div>
     </div>
   </div>
@@ -39,6 +44,8 @@
 <script setup>
 import IconMinus from '@/components/atoms/icons/IconMinus.vue';
 import LineChartAtom from '@/components/atoms/stats/LineChartAtom.vue';
+import IconEmptyStat from '@/components/atoms/icons/IconEmptyStat.vue';
+import { computed } from 'vue';
 
 const props = defineProps({
   title: { type: String, required: true },
@@ -47,6 +54,14 @@ const props = defineProps({
   positive: { type: Boolean, required: true },
   chartData: { type: Array, default: () => [] },
   editMode: Boolean,
+});
+
+const isNotData = computed(() => {
+  return true;
+  // const valEmpty = !props.value || props.value === 0;
+  // const percentEmpty = !props.percent || props.percent === 0;
+  // const chartEmpty = !props.chartData || props.chartData.length === 0;
+  // return valEmpty && percentEmpty && chartEmpty;
 });
 </script>
 
@@ -90,8 +105,8 @@ const props = defineProps({
   align-items: flex-end;
 }
 
-.stat-widget-card__chart-bg {  
-  height: 68%;
+.stat-widget-card__chart-bg {
+  height: 70%;
   border-radius: $radius-lg;
   z-index: 1;
   pointer-events: none;
@@ -108,9 +123,9 @@ const props = defineProps({
 
 .stat-widget-card__info {
   position: absolute;
-  left: 16px;
+  left: 10px;
   right: 16px;
-  bottom: 16px;
+  bottom: 58px;
   z-index: 2;
   display: flex;
   flex-direction: column;
@@ -172,6 +187,10 @@ const props = defineProps({
   &--negative {
     color: #F04438;
   }
+
+  &--empty {
+    color: $gray-400;
+  }
 }
 
 .stat-widget-card__arrow {
@@ -204,5 +223,41 @@ const props = defineProps({
   border-radius: 50%;
   box-shadow: $shadow-main;
   z-index: 10;
+}
+
+.stat-widget-card__empty-center {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  pointer-events: none;
+  z-index: 3;
+}
+
+.stat-widget-card__empty-badge {
+  background: #FFFFFF;
+  color: #16181D;
+  font-size: 14px;
+  font-weight: 500;
+  border-radius: 20px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.04);
+  padding: 4px 11px;
+  margin-top: 34%;
+  text-align: center;
+  display: inline-block;
+}
+
+// .stat-widget-card__content--empty { 
+//   background: $color-bg-main !important;
+// }
+.stat-widget-card__chart-bg--empty {
+  height: 68%;
+  background: $color-bg-main !important;
 }
 </style>
